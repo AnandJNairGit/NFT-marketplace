69488bcd5685d0cd73fbb10b006c1e2bcfb9c5b1
AnandJNairGit <anandjnairit@gmail.com>
2023-03-15 03:55:00 +0530

    create marketplace nav bar

---
+++ src/components/navbar/index.js
@@ -0,0 +1,145 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+import { ContractContext } from "../../App";
+
+const drawerWidth = 240;
+
+function NavBar(props) {
+  const { isAdmin, hasAdminAccess } =
+    React.useContext(ContractContext).contractConfig;
+  const navItems =
+    isAdmin || hasAdminAccess
+      ? ["Home", "Marketplace", "Profile", "Admin"]
+      : ["Home", "Marketplace", "Profile", ];
+  const navRoutes =
+    isAdmin || hasAdminAccess
+      ? ["", "marketplace", "profile", "admin"]
+      : ["", "marketplace", "profile", ];
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const navigate = useNavigate();
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        WEB 3
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item, index) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton
+              sx={{ textAlign: "center" }}
+              onClick={() => {
+                navigate(`/${navRoutes[index]}`);
+              }}
+            >
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        component="nav"
+        sx={{
+          background:
+            "linear-gradient(to right, #f25d0e, #ec650b, #e66d0b, #e0730d, #da7911)",
+          position: "fixed",
+          top: 0,
+          // zIndex: 1,
+          width: "100%",
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            WEB 3
+          </Typography>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            {navItems.map((item, index) => (
+              <Button
+                key={item}
+                sx={{ color: "#fff" }}
+                onClick={() => {
+                  navigate(`/${navRoutes[index]}`);
+                }}
+              >
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </Box>
+  );
+}
+
+NavBar.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default NavBar;
